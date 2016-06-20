247cb61f499acfe1d25d92595a67c430efca77e4
Eduardo Destruti <eduardo.destruti@maquinadobem.com>
2016-06-20 03:44:45 -0300

    fix

--- src/class/toolbox.php
+++ src/class/toolbox.php
@@ -2,21 +2,43 @@
 
 class toolbox {
 
+    /**
+     * @return string
+     * @env integration
+     * SET COMMERCEID SEND FROM TRANSBANK SUPORT
+     */
     public static function getCommerceId() {
         return '597020000403';
     }
 
+    /**
+     * @return string
+     * @env integration
+     * SET INTEGRATION WS LINK SEND FROM TRANSBANK SUPORT
+     */
     public static function getUrlWsdl() {
         return 'https://webpay3gint.transbank.cl/WSWebpayTransaction/cxf/WSWebpayService?wsdl';
     }
 
+    /**
+     * @return string
+     * @env integration
+     * PATH TO crt FILE SEND FROM TRANSBANK SUPORT
+     */
     public static function getCert() {
         return $_SERVER["DOCUMENT_ROOT"].'/src/certs/597020000403.crt';
     }
 
+    /**
+     * @return string
+     * @env integration
+     * PATH TO key FILE SEND FROM TRANSBANK SUPORT
+     */
     public static function getCertKey() {
         return $_SERVER["DOCUMENT_ROOT"].'/src/certs/597020000403.key';
     }
+
+    /** DONT NEED TO CHANGE AT THIS POINT */
 
     public static function makeReturnUrl($buyOrder) {
         return self::website().'src/returnUrl.php?status=success&buyOrder='.$buyOrder;
